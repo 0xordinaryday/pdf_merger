0df405544b50a12e712a6a015837f2b500487844
David <david@gibbons.digital>
2022-03-07 21:09:50 +1100

    started to implement images; replace partially implemented

--- PDFEditor.py
+++ PDFEditor.py
@@ -5,10 +5,11 @@ import PyPDF2
 import sys
 import os
 import re
+from PIL import Image, ImageTk
 
 # root window
 root = tk.Tk()
-root.geometry('500x400')
+root.geometry('700x400')
 root.wm_title('PDF Editor')
 
 # Create a style
@@ -19,7 +20,7 @@ style.theme_use('winnative')
 # winnative, clam and alt are ok
 
 # create a notebook
-notebook = ttk.Notebook(root)
+notebook = ttk.Notebook(root,height=400,width=700)
 notebook.pack(pady=10, expand=True)
 
 ##############################################
@@ -29,10 +30,12 @@ def AddFileShared(flag):
     # shared with explode, pass flag to EESetText
     ftypes = [('PDF files', '*.pdf'), ('All files', '*')]
     filename = filedialog.askopenfilename() 
-    if flag < 3:
+    if flag == 'extract' or flag == 'explode':
         EESetText(filename, flag)
-    if flag == 3:
+    elif flag == 'delete':
         SetDeleteText(filename)
+    else:
+        SetReplaceText(filename, flag)
     
 def CountPages(filepath):
     # must be a PDF
@@ -60,15 +63,14 @@ def EESetText(filepath, flag):
     # shared with explode, flag controls behaviour
     filename, extension = os.path.splitext(filepath)
     var_to_set = ''
-    which_function = ''
-    if flag == 1:
-        var_to_set, which_function = extract_label, 'extract'
-    elif flag == 2:
-        var_to_set, which_function = explode_label, 'explode'
+    if flag == 'extract':
+        var_to_set = extract_label
+    elif flag == 'explode':
+        var_to_set = explode_label
     if extension.lower() != '.pdf':
-        var_to_set.set("File is: {} \nFile is not a PDF, cannot {}.".format(filepath, which_function))
+        var_to_set.set("File is: {} \nFile is not a PDF, cannot {}.".format(filepath, flag))
     elif CountPages(filepath) == 1:
-        var_to_set.set("File is: {} \nThis only has one page, so there is nothing to {}.".format(filepath, which_function))
+        var_to_set.set("File is: {} \nThis only has one page, so there is nothing to {}.".format(filepath, flag))
     else:
         if flag == 1:
             var_to_set.set("File is: {} \nFile has {} pages, which page should I extract?".format(filepath, CountPages(filepath)))
@@ -206,6 +208,66 @@ def DoDelete():
     
 # End delete functions
 ##############################################     
+
+##############################################
+# Replace functions
+
+def SetReplaceText(filepath, flag):
+    filename, extension = os.path.splitext(filepath)
+    if flag == 'replace_into':
+        if extension.lower() != '.pdf':
+            replace_label.set("File is: {} \nFile is not a PDF, please choose a PDF.".format(filepath))
+        elif CountPages(filepath) == 1:
+            replace_label.set("File is: {} \nThis only has one page, so replacing it doesn't make sense.".format(filepath))
+        else:
+            countOfPages = CountPages(filepath)
+            replace_label.set("File chosen to have a page replaced is:\n{}\nThe file has {} pages, choose page to replace.".format(filepath, countOfPages))
+            SetPages(countOfPages, replaceCombo_replacee)
+    else: # 'replace_from'
+        return None
+    #if extension.lower() != '.pdf':
+    #    delete_label.set("File is: {} \nFile is not a PDF, cannot delete.".format(filepath))
+    #elif CountPages(filepath) == 1:
+    #    delete_label.set("File is: {} \nThis only has one page, so there is nothing to delete.".format(filepath))
+    #else:
+    #    countOfPages = CountPages(filepath)
+    #    delete_label.set("File is: {} \nFile has {} pages, set pages to delete.".format(filepath, countOfPages))
+    #    SetPages(countOfPages, deleteCombo_start)
+    #    SetPages(countOfPages, deleteCombo_end)
+        
+def DoDelete():
+    # check if valid
+    try:
+        startnumber = int(deleteCombo_start.get())
+        endnumber = int(deleteCombo_end.get())
+    except ValueError as ve:
+        tk.messagebox.showinfo(title='Alert', message='Set yo pages') 
+        return None
+    if endnumber < startnumber:
+        tk.messagebox.showinfo(title='Alert', message='Last page to delete cannot be lower than first page!') 
+        return None
+    
+    # assuming validity    
+    save_filename = filedialog.asksaveasfilename()
+    textbox_text = delete_label.get()
+    pattern = "File is: (.*?)\n"
+    filepath = re.search(pattern, textbox_text).group(1)
+    
+    fname = os.path.splitext(os.path.basename(filepath))[0]
+    directory = os.path.dirname(filepath) + '/'
+    
+    pdf = PyPDF2.PdfFileReader(filepath)
+    pdf_writer = PyPDF2.PdfFileWriter()
+    for page in range(pdf.getNumPages()):
+        if page +1 < startnumber or page +1 > endnumber: 
+            pdf_writer.addPage(pdf.getPage(page))
+            with open(save_filename, 'wb') as out:
+                pdf_writer.write(out)
+
+    tk.messagebox.showinfo(title='Alert', message='File written.')        
+    
+# End Replace functions
+##############################################     
     
         
 class DragDropListbox(tk.Listbox):
@@ -235,11 +297,11 @@ class DragDropListbox(tk.Listbox):
             self.curIndex = i    
 
 # create frames
-extract = ttk.Frame(notebook, width=500, height=350)
-merge = ttk.Frame(notebook, width=500, height=350)
-replace = ttk.Frame(notebook, width=500, height=350)
-delete = ttk.Frame(notebook, width=500, height=350)
-explode = ttk.Frame(notebook, width=500, height=350)
+extract = ttk.Frame(notebook, width=700, height=350)
+merge = ttk.Frame(notebook, width=700, height=350)
+replace = ttk.Frame(notebook, width=700, height=350)
+delete = ttk.Frame(notebook, width=700, height=350)
+explode = ttk.Frame(notebook, width=700, height=350)
 
 extract.grid(column=0, row=0, padx=15, pady=15)
 merge.grid(column=0, row=0, padx=15, pady=15)
@@ -285,7 +347,7 @@ merge_filename_box.grid(row=3, column=0, columnspan=2)
 
 closeButton = ttk.Button(extract, text="Clear", command=lambda: ClearFilesFromExtract(extractCombo, extract_label))
 closeButton.grid(row=1, column=1, padx=(10,10), pady=(10, 10), sticky = tk.W)   
-addFileButton = ttk.Button(extract, text="Add File", command=lambda: AddFileShared(1)) # Need lambda to pass argument
+addFileButton = ttk.Button(extract, text="Add File", command=lambda: AddFileShared('extract')) # Need lambda to pass argument
 addFileButton.grid(row=1, column=0, padx=(10,10), pady=(10, 10), sticky = tk.W)   
 extractButton = ttk.Button(extract, text="Extract", command = DoExtraction)
 extractButton.grid(row=5, column=0, padx=(10,10), pady=(10, 10), sticky = tk.W)   
@@ -308,13 +370,20 @@ extractCombo = ttk.Combobox(extract, values = vlist)
 extractCombo.set("Choose a page")
 extractCombo.grid(row=4, column=0, padx=(10,10), pady=(10, 10), sticky = tk.W+tk.E)
 
+## Woo, let's try a picture in a button
+myimg = Image.open("output_01.jpg")
+myimg = myimg.resize((197, 278), Image.ANTIALIAS)
+myimg = ImageTk.PhotoImage(myimg)
+addImageButton = tk.Button(extract, image=myimg, width=200, height=300, command=None) 
+addImageButton.grid(row=1, column=4, padx=(10,10), pady=(10, 10), rowspan=10, columnspan=10)
+
 # End Extract GUI elements
 ##############################################
 
 ##############################################
 # Explode GUI elements
 
-addFileButton = ttk.Button(explode, text="Add File", command=lambda: AddFileShared(2)) # Need lambda to pass argument
+addFileButton = ttk.Button(explode, text="Add File", command=lambda: AddFileShared('explode')) # Need lambda to pass argument
 addFileButton.grid(row=1, column=0, padx=(10,10), pady=(10, 10), sticky = tk.W)   
 explodeButton = ttk.Button(explode, text="Explode", command = DoExplode)
 explodeButton.grid(row=5, column=0, padx=(10,10), pady=(10, 10), sticky = tk.W)   
@@ -337,7 +406,7 @@ label.grid(row=3, column=0, padx=(10,10), pady=(10, 10), columnspan=2, sticky = 
 ##############################################
 # Delete GUI elements
 
-addFileButton = ttk.Button(delete, text="Add File", command=lambda: AddFileShared(3)) # 
+addFileButton = ttk.Button(delete, text="Add File", command=lambda: AddFileShared('delete')) # 
 addFileButton.grid(row=1, column=0, padx=(10,10), pady=(10, 10), sticky = tk.W)   
 deleteButton = ttk.Button(delete, text="Delete", command = DoDelete) 
 deleteButton.grid(row=5, column=0, padx=(10,10), pady=(10, 10), sticky = tk.W)   
@@ -364,7 +433,42 @@ deleteCombo_end.set("Last page to delete")
 deleteCombo_start.grid(row=4, column=0, padx=(10,10), pady=(10, 10), sticky = tk.W+tk.E)
 deleteCombo_end.grid(row=4, column=1, padx=(10,10), pady=(10, 10), sticky = tk.W+tk.E)
 
-# End Extract GUI elements
+# End Delete GUI elements
+##############################################
+
+##############################################
+# Replace GUI elements
+
+addFileButton_into = ttk.Button(replace, text="File needing page replaced", command=lambda: AddFileShared('replace_into'))
+addFileButton_into.grid(row=1, column=0, padx=(10,10), pady=(10, 10), sticky = tk.W)  
+addFileButton_from = ttk.Button(replace, text="File with replacement page", command=lambda: AddFileShared('replace_from'))
+addFileButton_from.grid(row=1, column=1, padx=(10,10), pady=(10, 10), sticky = tk.W)   
+replaceButton = ttk.Button(replace, text="Replace", command = None) 
+replaceButton.grid(row=5, column=0, padx=(10,10), pady=(10, 10), sticky = tk.W)   
+
+# Add information text to replace
+replace_information = tk.StringVar()
+replace_information.set("This function allows for the replacement of a page with one from another document")
+label = tk.Label(replace, textvariable = replace_information, justify=tk.LEFT, anchor="w")
+label.grid(row=2, column=0, padx=(10,10), pady=(10, 10), columnspan=4, sticky = tk.W+tk.E)
+
+# Add label to replace
+replace_label = tk.StringVar()
+replace_label.set("Choose the files using the buttons above\nThe file which needs a replacement page is selected with the button on the left\nThe file which will provide the replacement page is selected with the button on the right")
+label = tk.Label(replace, textvariable = replace_label, justify=tk.LEFT, anchor="w")
+label.grid(row=3, column=0, padx=(10,10), pady=(10, 10), columnspan=2, sticky = tk.W+tk.E)
+
+# Add comboboxes for pagenumbers to replace
+replace_vlist_from = []
+replace_vlist_to = []
+replaceCombo_replacee = ttk.Combobox(replace, values = vlist)
+replaceCombo_replacer = ttk.Combobox(replace, values = vlist)
+replaceCombo_replacee.set("The page to be replaced")
+replaceCombo_replacer.set("The replacement page")
+replaceCombo_replacee.grid(row=4, column=0, padx=(10,10), pady=(10, 10), sticky = tk.W+tk.E)
+replaceCombo_replacer.grid(row=4, column=1, padx=(10,10), pady=(10, 10), sticky = tk.W+tk.E)
+
+# End Replace GUI elements
 ##############################################
 
 png_base64_string = """iVBORw0KGgoAAAANSUhEUgAAAJYAAABWCAIAAACii/gBAAABhGlDQ1BJQ0MgcHJvZmlsZQAAKJF9kT1Iw0AcxV8TtSIVBzuIOGSoThb8QhylikWwUNoKrTqYXPohNGlIUlwcBdeCgx+LVQcXZ10dXAVB8APE0clJ0UVK/F9SaBHjwXE/3t173L0DhHqZaVbHGKDptpmKx6RsbkUKvkJEFwSMQ5SZZSTSCxn4jq97BPh6F+VZ/uf+HL1q3mJAQCKeZYZpE68TT2/aBud94jArySrxOfGoSRckfuS64vEb56LLAs8Mm5nUHHGYWCq2sdLGrGRqxFPEEVXTKV/Ieqxy3uKslauseU/+wlBeX05zneYQ4lhEAklIUFDFBsqwEaVVJ8VCivZjPv5B158kl0KuDTByzKMCDbLrB/+D391ahckJLykUAzpfHOdjGAjuAo2a43wfO07jBBCfgSu95a/UgZlP0mstLXIE9G0DF9ctTdkDLneAgSdDNmVXEmkKhQLwfkbflAP6b4GeVa+35j5OH4AMdbV0AxwcAiNFyl7zeXd3e2//nmn29wMsp3KLNIn8WwAAAAlwSFlzAAAuIwAALiMBeKU/dgAAAAd0SU1FB+YDBgI5I8FlX5UAAAAZdEVYdENvbW1lbnQAQ3JlYXRlZCB3aXRoIEdJTVBXgQ4XAAAAvUlEQVR42u3dMQ6AIAxAUWuce//blgPUgbGQ90fGvqAxJhBV9ejkXiNAKIRCiFBn9/WliDCXsfUvCLvQg1QIhRChEAqhECIUQiEUQoRCKIRCiFAIhVAIEQqhEAohQiEUQiFEKIRCKIQIhVAIhRChEAqhECIUQiEUQoRCKIRCiFAIhVAIEeoOwsw0o+FFPzTfJQeTc8mBd6EQCqEQIhRCIRRChEIohEKIUAiFUAgv6uevvexCIRRChEIohNpvARgwD823LLzRAAAAAElFTkSuQmCC"""
